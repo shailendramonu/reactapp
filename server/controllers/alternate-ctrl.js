c08e8fcb8f0c12b66b636f770aeb47b2df967a27
Shailendra Singh <shailendra.singh@transport.nsw.gov.au>
2020-06-26 18:59:47 +1000

    initial commit

---
+++ server/controllers/alternate-ctrl.js
@@ -0,0 +1,122 @@
+const Alternate = require('../models/alternate-model')
+
+createAlternate = (req, res) => {
+  const body = req.body
+
+  if (!body) {
+    return res.status(400).json({
+      success: false,
+      error: 'You must provide a alternate',
+    })
+  }
+
+  const alternate = new Alternate(body)
+
+  if (!alternate) {
+    return res.status(400).json({ success: false, error: err })
+  }
+
+  alternate
+    .save()
+    .then(() => {
+      return res.status(201).json({
+        success: true,
+        id: alternate._id,
+        message: 'Alternate created!',
+      })
+    })
+    .catch(error => {
+      return res.status(400).json({
+        error,
+        message: 'Alternate not created!',
+      })
+    })
+}
+
+updateAlternate = async (req, res) => {
+  const body = req.body
+
+  if (!body) {
+    return res.status(400).json({
+      success: false,
+      error: 'You must provide a body to update',
+    })
+  }
+
+  Alternate.findOne({ _id: req.params.id }, (err, alternate) => {
+    if (err) {
+      return res.status(404).json({
+        err,
+        message: 'Alternate not found!',
+      })
+    }
+    alternate.appId = body.appId
+    alternate.title = body.title
+    alternate.icon = body.icon
+    alternate.genres = body.genres
+    alternate.region = body.region
+    alternate
+      .save()
+      .then(() => {
+        return res.status(200).json({
+          success: true,
+          id: alternate._id,
+          message: 'Alternate updated!',
+        })
+      })
+      .catch(error => {
+        return res.status(404).json({
+          error,
+          message: 'Alternate not updated!',
+        })
+      })
+  })
+}
+
+deleteAlternate = async (req, res) => {
+  await Alternate.findOneAndDelete({ _id: req.params.id }, (err, alternate) => {
+    if (err) {
+      return res.status(400).json({ success: false, error: err })
+    }
+
+    if (!alternate) {
+      return res
+        .status(404)
+        .json({ success: false, error: `Alternate not found` })
+    }
+
+    return res.status(200).json({ success: true, data: alternate })
+  }).catch(err => console.log(err))
+}
+
+getAlternateById = async (req, res) => {
+  await Alternate.findOne({ _id: req.params.id }, (err, alternate) => {
+    if (err) {
+      return res.status(400).json({ success: false, error: err })
+    }
+
+    return res.status(200).json({ success: true, data: alternate })
+  }).catch(err => console.log(err))
+}
+
+getAlternates = async (req, res) => {
+  await Alternate.find({}, (err, alternates) => {
+    if (err) {
+      return res.status(400).json({ success: false, error: err })
+    }
+    if (!alternates.length) {
+      return res
+        .status(404)
+        .json({ success: false, error: `Alternate not found` })
+    }
+    return res.status(200).json({ success: true, data: alternates })
+  }).catch(err => console.log(err))
+}
+
+module.exports = {
+  createAlternate,
+  updateAlternate,
+  deleteAlternate,
+  getAlternates,
+  getAlternateById,
+}
---
+++ server/routes/alternate-router.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const AlternateCtrl = require('../controllers/alternate-ctrl')
+
+const router = express.Router()
+
+router.post('/alternate', AlternateCtrl.createAlternate)
+router.put('/alternate/:id', AlternateCtrl.updateAlternate)
+router.delete('/alternate/:id', AlternateCtrl.deleteAlternate)
+router.get('/alternate/:id', AlternateCtrl.getAlternateById)
+router.get('/alternates', AlternateCtrl.getAlternates)
+
+module.exports = router
